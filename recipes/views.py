713b11efdb85fc8ec193faaefad19bfa5671f4c4
Mateus Soares <soares.cmateus@gmail.com>
2023-12-10 20:16:16 -0300

    templetes exemple

---
+++ recipes/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def home(request):
+    return render(request, 'recipes/home.html', context={
+        'name': 'Mateus Soares',
+    })
+
+
+def sobre(request):
+    return HttpResponse('Novo Sobre')
+
+
+def contato(request):
+    return HttpResponse('Novo Contato')
+
