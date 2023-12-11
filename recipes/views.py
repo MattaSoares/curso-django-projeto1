18ee94f296184ec5a93cdab4ba4aebacff68a3ce
Mateus Soares <soares.cmateus@gmail.com>
2023-12-11 14:19:07 -0300

    urls contato and sobre removed

--- recipes/views.py
+++ recipes/views.py
@@ -1,17 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
     return render(request, 'recipes/home.html', context={
         'name': 'Mateus Soares',
     })
-
-
-def sobre(request):
-    return HttpResponse('Novo Sobre')
-
-
-def contato(request):
-    return render(request, 'recipes/contato.html')
 
