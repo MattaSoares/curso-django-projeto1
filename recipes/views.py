58e995ebb151b013888b626b8fe3fc27dd2be8d5
Mateus Soares <soares.cmateus@gmail.com>
2023-12-28 14:27:53 -0300

    faker

--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 # Create your views here.
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Mateus Soares',
+        'recipes': [make_recipe() for _ in range(10)],
     })
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'name': 'Mateus Soares',
+        'recipe': make_recipe(),
     })
